46c27361c2923326daf75386146ae6173783ac0b
Laura Cole <lauracole1900@comcast.net>
2021-10-16 18:13:34 -0600

    added email, password stuff, bcrypt hashing to User model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
-
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection.js');
 
-class User extends Model { }
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -15,11 +19,29 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
     password: {
-      // store hashed password using bcrypt
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      }
     }
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
