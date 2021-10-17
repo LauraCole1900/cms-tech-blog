5fae2a1e0a7a028675bafe3d9152aec03c98c815
Laura Cole <lauracole1900@comcast.net>
2021-10-16 21:22:49 -0600

    drafted seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,38 @@
+const sequelize = require('../config/connection');
+const { Blogpost, Comment, User } = require('../models');
+
+const blogSeedData = require('./blogSeedData.json');
+const commentSeedData = require('./commentSeedData.json');
+const userSeedData = require('./userSeedData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userSeedData);
+
+  for (const { id } of users) {
+    const newLicense = await License.create({
+      driver_id: id,
+    });
+  }
+
+  for (const post of blogSeedData) {
+    const newPost = await Blogpost.create({
+      ...post,
+      // Attach a random user ID to each car
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentSeedData) {
+    const newComment = await Comment.create({
+      ...comment,
+      // Attach a random user ID to each comment
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
