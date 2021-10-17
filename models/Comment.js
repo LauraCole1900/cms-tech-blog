cfd34bdb1e68d30321905665e0ecbf1455e720bc
Laura Cole <lauracole1900@comcast.net>
2021-10-16 18:03:51 -0600

    drafted Comment model

---
+++ models/Comment.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'user',
+        key: 'id',
+        unique: false
+      }
+    },
+    blogpost_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blogpost',
+        key: 'id',
+        unique: false
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
