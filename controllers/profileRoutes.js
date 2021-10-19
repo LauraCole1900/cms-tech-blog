ee441b458578cbf002c959482170c9acac8aea77
Laura Cole <lauracole1900@comcast.net>
2021-10-18 20:30:17 -0600

    added login route to profileRoutes

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -4,5 +4,22 @@ const withAuth = require('../utils/auth');
 
 // '/profile' stem
 // GET blogposts by signed-in user's ID
+router.get('/', withAuth, async (req, res) => {
+  console.log(req.session);
+  try {
+  const userPosts = await Blogpost.findAll({
+    where: {
+      user_id: req.session.user_id
+    }
+  });
+  const posts = userPosts.map(post => post.get({ plain: true }));
+  res.render('user-profile', {
+    layout: 'profile',
+    posts
+  });
+} catch (err) {
+  res.redirect('login');
+}
+})
 
 module.exports = router;
